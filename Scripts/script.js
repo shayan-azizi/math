c6dc913d8bfb00b58013ddb00c95c0caa8460dc3
Shayan Azizi <80700762+shayan-azizi@users.noreply.github.com>
2021-12-30 19:27:11 +0330

    Add background, prime and GCD

---
+++ Scripts/script.js
@@ -0,0 +1,44 @@
+function BMMKMM () {
+    let firstNumber = document.getElementById("number1").value;
+    let secondNumber = document.getElementById("number2").value;
+
+    function BMM (a , b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return BMM (b, a % b);
+    }
+
+    function KMM (a, b) {
+        number = BMM (a , b);
+        return a * b / number;
+    }
+
+    document.getElementById("b-m-m").innerHTML ="ب.م.م برابر است با: " + BMM(firstNumber, secondNumber);
+    document.getElementById("k-m-m").innerHTML = "ک.م.م برابر است با: " + KMM(firstNumber, secondNumber);
+    
+
+}
+
+function isPrime () {
+    number = document.getElementById("number3").value
+
+    if (number > 1) {
+
+        for (let i = 2; i < Math.round(number / 2) + 1; i++) {
+            
+            if (number % i == 0) {
+                document.getElementById("prime").innerHTML = "عدد اول نیست!";
+                break;
+            }
+
+            else {
+                document.getElementById("prime").innerHTML = "عدد اول است...";
+            }
+        } 
+    }
+
+    else {
+        document.getElementById("prime").innerHTML = "عدد اول نیست!";
+    }
+}
